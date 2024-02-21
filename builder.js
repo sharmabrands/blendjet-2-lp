e843c0c5ed41ff1b5bda5801c8469864cdf42be8
Rohit Gupta <rohitgupta@Rohits-MacBook-Air.local>
2024-02-21 16:09:15 -0500

    mobile css fixes

--- builder.js
+++ builder.js
@@ -3,7 +3,7 @@ let { build } = require("esbuild");
 build({
   entryPoints: ["index.js"],
   outfile: "./dist/index.js",
-  minify: false,
+  minify: true,
   bundle: true,
   sourcemap: true,
 }).catch(() => process.exit(1));
